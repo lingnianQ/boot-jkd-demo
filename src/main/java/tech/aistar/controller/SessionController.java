cd99ed65878a385587712cb8a5930f5bd0cc7223
lingnian <sytsnb@gmail.com>
2021-11-06 12:43:49 +0800

    commitmes

---
+++ src/main/java/tech/aistar/controller/SessionController.java
@@ -0,0 +1,46 @@
+package tech.aistar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 本类用来演示: session作用域
+ *
+ * @author: success
+ * @date: 2021/11/6 9:33 上午
+ */
+@RestController
+@RequestMapping("/ses")
+public class SessionController {
+    /**
+     * localhost:8081/boot/ses/add?name=success
+     * 将数据放入到session作用域
+     * @return
+     */
+    @GetMapping("/add")
+    public String add(HttpServletRequest request,String name){
+        //获取session对象
+        //第一次执行,容器内部会开辟一块session空间
+        HttpSession session = request.getSession();
+        //存储的数据的格式是key-value
+        session.setAttribute("username",name);
+        return "add";
+    }
+
+    /**
+     * 从session作用域中获取数据
+     * @return
+     */
+    @GetMapping("/get")
+    public String get(HttpServletRequest request){
+        //获取session对象
+        HttpSession session = request.getSession();
+        //根据key来获取value
+        String result = (String) session.getAttribute("username");
+        return result;
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sytsn
+  Date: 2021/11/6
+  Time: 10:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:redirect url="/phone/index"></c:redirect>
+
+<%--<html>--%>
+<%--<head>--%>
+<%--    <title></title>--%>
+<%--    <meta charset="UTF-8">--%>
+<%--    <meta name="viewport" content="width=device-width, initial-scale=1.0">--%>
+<%--    <meta http-equiv="refresh" content="0.1;url=http://localhost:8081/boot/phone/index">--%>
+<%--</head>--%>
+<%--<body>--%>
+
+<%--</body>--%>
+<%--</html>--%>
